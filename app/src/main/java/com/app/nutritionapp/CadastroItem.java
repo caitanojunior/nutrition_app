97230523374d9ab0aeac51d92905b50856174891
caitanojunior <caitanojunior@gmail.com>
2016-08-29 22:48:10 -0300

    criação da tela cadastro

--- app/src/main/java/com/app/nutritionapp/CadastroItem.java
+++ app/src/main/java/com/app/nutritionapp/CadastroItem.java
@@ -1,8 +1,24 @@
 package com.app.nutritionapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by caitano on 8/29/16.
  */
-public class CadastroItem {
-    
+public class CadastroItem extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_item);
+    }
+
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        startActivity(intent);
+        return;
+    }
 }
