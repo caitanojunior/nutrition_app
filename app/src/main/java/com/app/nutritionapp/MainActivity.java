7ab0efd87ef6cafca6f9481eb03f33a0249e52fe
caitanojunior <caitanojunior@gmail.com>
2016-10-22 11:22:09 -0300

    update at strings of the layouts, now going to reference at file string.xml and change in color app

--- app/src/main/java/com/app/nutritionapp/MainActivity.java
+++ app/src/main/java/com/app/nutritionapp/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void exitApp(View view) {
-        finish();
+
+        finishAffinity();
     }
 }
--- app/src/main/java/com/app/nutritionapp/RegisterItem.java
+++ app/src/main/java/com/app/nutritionapp/RegisterItem.java
@@ -15,49 +15,10 @@ import android.widget.Toast;
 
 public class RegisterItem extends Activity {
 
-    Repository r;
-    Button save, search;
-    EditText name, quantity, kcal;
-    Spinner measure;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_item);
-
-        eventos();
-    }
-
-    // Inicialização dos componetes e Evento dos Botões
-    public void eventos() {
-        name = (EditText) findViewById(R.id.editTextName);
-        quantity = (EditText) findViewById(R.id.editTextQuant);
-        measure = (Spinner) findViewById(R.id.spinnerMeasure);
-        kcal = (EditText) findViewById(R.id.editTextKcal);
-        save = (Button) findViewById(R.id.ButtonRegister);
-        search = (Button)findViewById(R.id.ButtonSearch);
-        r = new Repository(this);
-
-        save.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Food a = new Food();
-                a.setName(name.getText().toString());
-                double quantityDouble = Double.parseDouble(quantity.getText().toString());
-                a.setQuantity(quantityDouble);
-                a.setMeasure(measure.getSelectedItem().toString());
-                r.insert(a);
-                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        search.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                r.disAll();
-                for(int i=0;i<r.cont;i++){
-                    Toast.makeText(getApplicationContext(), "ID: "+r.id[i]+"\nNome: "+r.name[i]+"\nQuant: "+r.quantity[i]+"\nKcal: "+r.kcal[i], Toast.LENGTH_LONG).show();
-                }
-            }
-        });
     }
 
     public void onBackPressed(View arg0) {
