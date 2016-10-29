594e3565aa740490419f6ba5339de59b691d8334
caitanojunior <caitanojunior@gmail.com>
2016-10-29 19:09:03 -0300

    removing the title bar on the splash screen

--- app/src/main/java/com/app/nutritionapp/Splash.java
+++ app/src/main/java/com/app/nutritionapp/Splash.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 /**
  * Created by caitano on 8/29/16.
@@ -12,9 +13,9 @@ public class Splash extends Activity implements Runnable {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-
         Handler handler = new Handler();
         handler.postDelayed(this, 3000);
     }
