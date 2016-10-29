c3f6d22b6f7a35fdee04fbc1ea2e12fdbd7930bc
caitanojunior <caitanojunior@gmail.com>
2016-10-29 18:38:44 -0300

    implementation about screen

---
+++ app/src/main/java/com/app/nutritionapp/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.app.nutritionapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by caitano on 10/29/16.
+ */
+public class AboutActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
--- app/src/main/java/com/app/nutritionapp/MainActivity.java
+++ app/src/main/java/com/app/nutritionapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.app.nutritionapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                // go home
+                Intent intent = new Intent(this, AboutActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void cad_item(View view){
