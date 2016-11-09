32c04bf967757df8b7a7ce68ac7aa9556674f1c4
caitanojunior <caitanojunior@gmail.com>
2016-11-08 21:35:12 -0300

    translate of application to portuguese

--- app/src/main/java/com/app/nutritionapp/MainActivity.java
+++ app/src/main/java/com/app/nutritionapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.app.nutritionapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
