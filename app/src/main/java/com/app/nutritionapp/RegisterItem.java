5a93a3add4b237afddfe06bdc444afc226fa9926
Fabricio Lelis <fabricio.melo@virtus.ufcg.edu.br>
2016-11-18 16:47:52 -0300

    Fixed strings and variables names

--- app/src/main/java/com/app/nutritionapp/RegisterItem.java
+++ app/src/main/java/com/app/nutritionapp/RegisterItem.java
@@ -35,28 +35,27 @@ public class RegisterItem extends Activity {
         EditText getName = (EditText)findViewById(R.id.editTextName);
         EditText getQuantity = (EditText) findViewById(R.id.editTextQuant);
         Spinner mySpinner =  (Spinner) findViewById(R.id.spinnerMeasure);
-        EditText getKCAL = (EditText)findViewById(R.id.editTextKcal);
+        EditText getCalories = (EditText)findViewById(R.id.editTextKcal);
 
         String name = getName.getText().toString();
         String unit = mySpinner.getSelectedItem().toString();
         String response;
 
         int quantity = Integer.parseInt(getQuantity.getText().toString());
-        int kcal = Integer.parseInt(getKCAL.getText().toString());
+        int calories = Integer.parseInt(getCalories.getText().toString());
         long result;
 
-        result = crud.insertValues(name, quantity, unit, kcal);
+        result = crud.insertValues(name, quantity, unit, calories);
 
         if (result == -1)
-            response = "Error ao inserir registro";
+            response = "Erro ao inserir registro";
         else {
             response = "Registro inserido com sucesso";
             getName.getText().clear();
             getQuantity.getText().clear();
-            getKCAL.getText().clear();
+            getCalories.getText().clear();
             mySpinner.getAdapter();
         }
         Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-
     }
 }
